3960eb9ca2edfe205bd666da2122bcca16705141
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-07-29 18:19:50 -0500

    add first endpoint

---
+++ app/utils.py
@@ -0,0 +1,32 @@
+from pydub import AudioSegment
+import io
+import ffmpeg
+
+
+def get_audio_properties(audio_bytes:bytes):
+    '''
+        Function to extract audio properties
+    '''
+    audio = AudioSegment.from_file(io.BytesIO(audio_bytes))
+    duration_ms = len(audio)
+    channels = audio.channels
+    frame_rate = audio.frame_rate
+    sample_width = audio.sample_width
+    return {
+        "duration_ms": duration_ms,
+        "channels": channels,
+        "frame_rate": frame_rate,
+        "sample_width": sample_width,
+    }
+
+def convert_audio_compatibility(audio_bytes:bytes):
+    '''
+        Function to convert audio to a compatible format
+    '''
+    audio_manipulated = audio_bytes
+    # convert from mp4a to pcm_s16le 
+    pipe = ffmpeg.input("pipe:0", threads=0)
+    pipe = ffmpeg.output(pipe, "pipe:1", format="s16le", acodec="pcm_s16le", ac=1, ar="16k")
+    audio_manipulated, _ = pipe.run(input=audio_manipulated, capture_stdout=True, capture_stderr=True)
+
+    return audio_manipulated
--- app/views.py
+++ app/views.py
@@ -1,5 +1,51 @@
 from . import app
+import whisper
+import ffmpeg
+import torch
+from fastapi import (
+    UploadFile
+)
+from .utils import(
+    get_audio_properties,
+    convert_audio_compatibility
+)
+import numpy as np
 
 @app.get("/")
 async def root():
+    '''
+        Base endpoint to test if the server is running
+    '''
     return {"message": "Hello World"}
+
+model = whisper.load_model(
+    name="base"
+)
+
+
+@app.post('/transcribe')
+def transcribe(audio:UploadFile):
+    '''
+        Endpoint for transcribing audio files
+    '''
+    print(audio.content_type)
+    audio_codec = audio.content_type.split("/")[-1] if "/" in audio.content_type else None
+
+    print(audio_codec)
+    audio_bytes = audio.file.read()
+    audio_properties = get_audio_properties(audio_bytes)
+
+    if audio_properties.get('frame_rate') != 160000:
+        audio_bytes = convert_audio_compatibility(audio_bytes)
+
+    audio_array = np.frombuffer(audio_bytes, np.int16).astype(np.float32).flatten() / 32768.0
+
+    transcription = model.transcribe(
+        audio_array, 
+        language="es", 
+        fp16=False, 
+        verbose=True,
+    )
+    transcription['text'] = transcription['text'].strip()
+
+    return transcription
